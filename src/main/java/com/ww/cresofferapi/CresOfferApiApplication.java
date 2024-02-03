ab17a5a2b7a27c3281bf8ed50b22f7de6d089cb1
wwelch <wwelch@local.domain>
2024-02-02 20:48:09 -0800

    initial commit

---
+++ src/main/java/com/ww/cresofferapi/CresOfferApiApplication.java
@@ -0,0 +1,13 @@
+package com.ww.cresofferapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CresOfferApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CresOfferApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ww/cresofferapi/CresOfferApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ww.cresofferapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CresOfferApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
