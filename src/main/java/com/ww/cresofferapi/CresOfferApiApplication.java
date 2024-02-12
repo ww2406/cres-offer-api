6a6f54e371cd8e0174b74e0364d50fff66a1d150
wwelch <wwelch@local.domain>
2024-02-12 11:10:06 -0800

    fix formatting

--- src/main/java/com/ww/cresofferapi/CresOfferApiApplication.java
+++ src/main/java/com/ww/cresofferapi/CresOfferApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CresOfferApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CresOfferApiApplication.class, args);
     }
-
 }
