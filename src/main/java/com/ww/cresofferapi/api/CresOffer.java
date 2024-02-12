64c2a5bcdc85dbf93ae6cbed87bd630d5310c5d1
wwelch <wwelch@local.domain>
2024-02-12 11:09:51 -0800

    get around Gson optional issue and support 404 by returning Message and manually serializing to JSON using Jackson. https://github.com/spring-cloud/spring-cloud-function/issues/1083

--- src/main/java/com/ww/cresofferapi/api/CresOffer.java
+++ src/main/java/com/ww/cresofferapi/api/CresOffer.java
@@ -1,23 +1,43 @@
 package com.ww.cresofferapi.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ww.cresofferapi.domain.data.RespondentService;
-import com.ww.cresofferapi.domain.models.Respondent;
+import org.springframework.cloud.function.adapter.gcp.FunctionInvoker;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
+import java.net.HttpURLConnection;
 import java.util.function.Supplier;
 
 @Component
-public class CresOffer implements Supplier<Optional<Respondent>> {
+public class CresOffer implements Supplier<Message<String>> {
     private final RespondentService respondentService;
+    private final ObjectMapper objectMapper;
 
-    public CresOffer(RespondentService respondentService) {
+    public CresOffer(RespondentService respondentService, ObjectMapper objectMapper) {
         this.respondentService = respondentService;
+        this.objectMapper = objectMapper;
     }
 
     @Override
-    public Optional<Respondent> get()
+    public Message<String> get()
     {
-        return respondentService.getNextRespondentAndUpdateAvailability();
+        var respondent = respondentService.getNextRespondentAndUpdateAvailability();
+        if (respondent.isEmpty()) {
+            return MessageBuilder
+                    .withPayload("null")
+                    .setHeader(FunctionInvoker.HTTP_STATUS_CODE, HttpURLConnection.HTTP_NOT_FOUND)
+                    .build();
+        } else {
+            try {
+                return MessageBuilder
+                        .withPayload(objectMapper.writeValueAsString(respondent))
+                        .build();
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
